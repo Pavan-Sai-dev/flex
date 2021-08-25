23bb8139046c0cd0cd9f7779a1cee7c951727213
Gangadhar Jannu <gangadhar014@gmail.com>
2021-08-25 18:55:23 +0200

    Fixes

---
+++ app.js
@@ -0,0 +1,77 @@
+const a = ` <nav class="navbar">
+<div class="image-logo">
+  <img src="logo.png" class="logo" alt="logo" />
+</div>
+<ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">About</a></li>
+  <li><a href="#">blog</a></li>
+  <li><a href="#">setting</a></li>
+  <li><a href="#">feedback</a></li>
+</ul>
+</nav>
+<main class="container">
+<div class="card">
+  <div class="content">
+    <h3>kagitha</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>jhansi</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>pavan</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>sai</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>web</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+</main>
+<div class="container">
+<div class="card">
+  <div class="content">
+    <h3>kagitha</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>jhansi</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>pavan</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>sai</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+<div class="card">
+  <div class="content">
+    <h3>web</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</div>
+</div>`;
+document.write(a);
